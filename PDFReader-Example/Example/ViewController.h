5c899179d26170b2a3f5ff43227c11e4048cc6a0
wind <wind.like.the.man@icloud.com>
2019-03-28 08:17:26 +0800

    加入测试Demo。

---
+++ PDFReader-Example/Example/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PDFReader-Example
+//
+//  Created by jia on 2019/3/28.
+//  Copyright © 2019 jia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ PDFReader-Example/Example/ViewController.m
@@ -0,0 +1,24 @@
+//
+//  ViewController.m
+//  PDFReader-Example
+//
+//  Created by jia on 2019/3/28.
+//  Copyright © 2019 jia. All rights reserved.
+//
+
+#import "ViewController.h"
+#import <Masonry/Masonry.h>
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
